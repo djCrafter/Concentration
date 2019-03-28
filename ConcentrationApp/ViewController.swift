06e16347d24ef71dab4874a6e0a783b84ae6d922
Crafter <godhedin2000@gmail.com>
2019-03-28 14:15:26 +0200

    Initial Commit

---
+++ ConcentrationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationApp
+//
+//  Created by Crafter on 3/28/19.
+//  Copyright © 2019 Crafter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
